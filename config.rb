278f01c9a68bf0cfc9a127a88c96073d66cc00e0
anymoto <ana@castromartinez.com>
2016-01-10 14:20:19 -0600

    Automated commit at 2016-01-10 20:20:19 UTC by middleman-deploy 2.0.0-alpha

--- config.rb
+++ config.rb
@@ -37,17 +37,17 @@ end
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 end
 
 activate :deploy do |deploy|
   deploy.deploy_method = :git
   # Optional Settings
   deploy.remote = 'git@github.com:anymoto/anymoto.github.io.git'
-  # deploy.branch   = 'custom-branch' # default: gh-pages
+  deploy.branch = 'master'
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
