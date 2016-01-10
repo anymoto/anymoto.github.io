da7ce9e6572a9e7752b668d6f57ca8c37f614db2
anymoto <ana@castromartinez.com>
2016-01-10 14:46:28 -0600

    Build before deploy

--- config.rb
+++ config.rb
@@ -45,6 +45,7 @@ end
 
 activate :deploy do |deploy|
   deploy.deploy_method = :git
+  deploy.build_before = true
   # Optional Settings
   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
   # deploy.branch   = 'custom-branch' # default: gh-pages
