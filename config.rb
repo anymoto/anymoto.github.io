da22f4c7f304c57f9095f515deb28ee15af59863
anymoto <ana@castromartinez.com>
2016-01-10 10:45:36 -0600

    Automated commit at 2016-01-10 16:45:36 UTC by middleman-deploy 2.0.0-alpha

--- config.rb
+++ config.rb
@@ -46,7 +46,7 @@ end
 activate :deploy do |deploy|
   deploy.deploy_method = :git
   # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  deploy.remote = 'git@github.com:anymoto/anymoto.github.io.git'
   # deploy.branch   = 'custom-branch' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
